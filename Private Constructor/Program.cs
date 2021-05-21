b9ada7ae0ac042b9cc3d50a50ff1d861d8e805e9
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-21 14:18:40 +0530

    Add files via upload

---
+++ Private Constructor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Private_Constructor
+{
+    public class PrivateCon
+    {
+        public static int currentCount;                     //Only static members because static members can be called without creating the instance
+        private PrivateCon()                                //Private constructor without parameter
+        {
+            
+        }
+        private PrivateCon(int currentCount)                                //Private constructor with parameter
+        {
+            Console.WriteLine("Private Constructor");
+            PrivateCon.currentCount = currentCount;                          //cannot access it with this keyword
+        }
+
+        public static int IncrementCount()
+        {
+            return ++currentCount;
+        }
+    }
+    //class Class1 : PrivateCon                         //error PrivateCon.PrivateCon()' is inaccessible due to its protection level
+    //{
+    //}                               
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PrivateCon obj = new PrivateCon();              //error 'PrivateCon.PrivateCon()' is inaccessible due to its protection level
+            Console.WriteLine(PrivateCon.currentCount);
+            Console.WriteLine(PrivateCon.IncrementCount());
+            PrivateCon.currentCount = 75;
+            Console.WriteLine(PrivateCon.IncrementCount());
+            PrivateCon.currentCount = 10;
+            Console.WriteLine(PrivateCon.IncrementCount());
+        }
+    }
+}
