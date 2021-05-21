d7d0baff00fe38cd1f4130b3c1847f4caa5be718
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-21 14:13:52 +0530

    Add files via upload

---
+++ Default Constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Default_Constructor
+{
+    class DefaultCon
+    {
+        public int num;
+        public string name;
+        public DefaultCon()                    //this would be invoked while the object of that class created.
+        {
+            Console.WriteLine("Constructor Called");
+            name = "Ram";
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            DefaultCon DefaultCon1 = new DefaultCon();          //invoke default constructor.
+            Console.WriteLine(DefaultCon1.num);
+            Console.WriteLine(DefaultCon1.name);                //Default constructor provides the default values
+        }
+    }
+}
