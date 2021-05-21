8fa45d7d67798b64ccda0e79081bdbaf5b56df01
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-21 14:20:33 +0530

    Add files via upload

---
+++ Static Constructor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Static_Constructor
+{
+    class Program
+    {
+        static int x;
+        int y;
+        static Program()
+        {
+            Console.WriteLine("static constructor called");
+            Console.WriteLine("x={0}", x);
+            x = 10;
+            //y = 30;                                               //Only static fields are initialize
+            Console.WriteLine("x={0}", x);
+            //Console.WriteLine("y={0}",y);
+        }
+        public Program(int y)
+        {
+            Console.WriteLine("Non-static Constructor Called");
+            this.y = y;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main method called");                //static constructor is called before main method
+            Program p = new Program(20);                            //Non-static Conatructor called
+            Console.WriteLine(p.y);
+
+            Program p1 = new Program(50);                           //Non-static Conatructor called
+            Console.WriteLine(p1.y);
+
+            Console.WriteLine(x);                               //static field directly called
+            //Console.WriteLine(y);                               //An object reference is required for non-static
+
+        }
+    }
+}
