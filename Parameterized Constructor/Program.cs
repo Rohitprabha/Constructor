47178bcaf3d978ea65c252eaf20ba676c26e9404
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-21 14:15:41 +0530

    Add files via upload

---
+++ Parameterized Constructor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Parameterized_Constructor
+{
+    class ParameterizedCon
+    {
+        public int num;
+        public string name;
+        public ParameterizedCon(int num, string name)                    //this would be invoked while the object of that class created.
+        {
+            this.num = num;
+            this.name = name;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParameterizedCon ParameterizedCon1 = new ParameterizedCon(1,"Ram");          //invoke Parameterized constructor.
+            Console.WriteLine(ParameterizedCon1.num);
+            Console.WriteLine(ParameterizedCon1.name);
+
+            ParameterizedCon ParameterizedCon2 = new ParameterizedCon(2, "Rohit");          //invoke Parameterized constructor.
+            Console.WriteLine(ParameterizedCon2.num);
+            Console.WriteLine(ParameterizedCon2.name);
+        }
+    }
+}
